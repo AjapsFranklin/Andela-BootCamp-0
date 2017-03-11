4d6ca923b38fcd5a28b7a656f972c8ade43d9726
framky007 <ajapsfranklin@gmail.com>
2017-03-11 16:16:37 +0100

    Added Test Suite for FizzBuzz

--- Test/test.js
+++ Test/test.js
@@ -56,5 +56,53 @@ var myApp = require('../App/library.js');
   });
 
 
+/// TEST SUITE FOR FIZZBUZZ
+var myApp = require('../App/library.js');
+describe("Fizz Buzz tests ", function() {
+
+  it("should return `Fizz` for number divisible by 3", function() {
+    expect(myApp.fizzBuzz(3)).toBe('Fizz');
+  });
+
+  it("should return `Buzz` for number divisible by 5", function() {
+    expect(myApp.fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it("should return `FizzBuzz` for 15", function() {
+    expect(myApp.fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 45", function() {
+    expect(myApp.fizzBuzz(45)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 90", function() {
+    expect(myApp.fizzBuzz(90)).toBe('FizzBuzz');
+  });
+
+  it("should return `Fizz` for 63", function() {
+    expect(myApp.fizzBuzz(63)).toBe('Fizz');
+  });
+
+  it("should return 7 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(7)).toBe(7);
+  });
+
+  it("should return 101 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(101)).toBe(101);
+  });
+
+  it("should return 0 since is not indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(0)).toBe(0);
+  });
+
+  it("should return -15 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(-15)).toBe("FizzBuzz");
+  });
+
+});
+
+
+
 })();
 
